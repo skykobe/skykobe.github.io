b185d5da7e1d13f978ddc1dea38cc655515096cd
Ricardo <lacushui123@163.com>
2016-07-15 11:29:13 +0800

    change my main page

--- js/controller.js
+++ js/controller.js
@@ -19,29 +19,95 @@ function RicardoPage() {
 	root.style.background = 'threedface'
 	root.style.backgroundSize = '120%'
 	wait.happend(w)
-	console.log(wait)
 	setTimeout(function() {		
 		afterLoad(['/image/18518352_153416231114_2.jpg', '/image/ricardo.jpg', '/image/music.png'], 'image', function() {
-			afterLoad('/resources/bgMusic.mp3', 'music', function() {
-				wait.end(w)
-				root.style.background = 'url(/image/18518352_153416231114_2.jpg)'
-				root.style.backgroundSize = '100%'
-				inserBgm()
-				var num = 3;
-				var div = document.createElement('div')
-				var p = document.createElement('p')
-				p.innerText = 'Now wait ' + num + ' Second'
-				var userLogo = new Image()
-				userLogo.src = '/image/ricardo.jpg'
-				userLogo.style.width = '200px'
-				div.className = 'class1'
-				div.innerHTML = 'I am front-end dog<br />'
-				div.appendChild(userLogo)
-				div.appendChild(p)
-				waitWordCss(p, num)
-				root.appendChild(div)
-			})		
-		})
+			wait.end(w)
+			root.style.background = 'url(/image/18518352_153416231114_2.jpg)'
+			root.style.backgroundSize = '100%'
+			var menuBar = '<div class="menuOut"><center class="word-menu">Click</center>' +
+						  '<div class="r1"><center class="word-menu">M</center></div>' +
+						  '<div class="r2"><center class="word-menu">W</center></div>' +
+						  '<div class="r3"><center class="word-menu">T</center></div></div>'     
+			root.innerHTML = menuBar
+			var t = document.querySelector('.menuOut');
+		    var url;
+		    var bar = ['translate(50%, 0%)', 'translate(50%, -100%)', 'translate(50%, -200%)']
+		    t.addEventListener('mousedown', function() {
+		      this.style.background = '#7fffd4'
+		      var r1 = document.querySelector('.r1');     
+		      var r2 = document.querySelector('.r2');      
+		      var r3 = document.querySelector('.r3');   
+		      r1.style.visibility = ''
+      		  r2.style.visibility = ''
+      		  r3.style.visibility = ''   
+		      r1.style.opacity = 0.7;
+		      r1.style.transform = 'translate(-120%, 80%)'
+		      r2.style.opacity = 0.7;
+		      r2.style.transform = 'translate(220%, -20%)'
+		      r3.style.opacity = 0.7;
+		      r3.style.transform = 'translate(50%, -380%)'
+		      r1.addEventListener('mouseover', function() {
+		        this.style.opacity = 1
+		        this.style.background = '#7fffef'
+		        url = 'music'
+		        root.className = 'Mroot'
+		      })
+		      r1.addEventListener('mouseout', function() {
+		        this.style.opacity = 0.7;
+		        this.style.background = '#7fffd4';
+		        url = 'RicardoPage'
+		      })
+		      r2.addEventListener('mouseover', function() {
+		        this.style.opacity = 1
+		        this.style.background = '#f0e6ee'
+		        url = 'wall'
+		        root.className = 'Wroot'
+		      })
+		      r2.addEventListener('mouseout', function() {
+		        this.style.opacity = 0.7
+		        this.style.background = '#f0e68c'
+		        url = 'RicardoPage'
+		      })
+		      r3.addEventListener('mouseover', function() {
+		        this.style.opacity = 1;
+		        this.style.background = '#fa80bb'
+		        url = 'word'
+		        root.className = 'Troot'
+		      })
+		      r3.addEventListener('mouseout', function() {
+		        this.style.opacity = 0.7
+		        this.style.background = '#fa8072'
+		        url = 'RicardoPage'
+		      })
+		    })
+
+			document.onmouseup = function() {
+		      document.onmousemove = null;
+		      t.style.background = '#00ffff'
+		      var r1 = document.querySelector('.r1');     
+		      var r2 = document.querySelector('.r2');      
+		      var r3 = document.querySelector('.r3');
+		      r1.style.visibility = 'hidden'
+		      r1.style.opacity = 0;
+		      r1.style.background = '#7fffd4'
+		      r1.style.transform = bar[0]
+		      r2.style.visibility = 'hidden'
+		      r2.style.opacity = 0;
+		      r2.style.background = '#f0e68c'
+		      r2.style.transform = bar[1]
+		      r3.style.visibility = 'hidden'
+		      r3.style.opacity = 0;
+		      r3.style.transform = bar[2]  
+		      if(url != 'RicardoPage') {
+		      	location.hash = url
+		      	root.style.transform = 'scale(2)'
+		      	root.style.background = 'black'
+		      } else {
+		      	root.className = ''
+		      }
+		    }
+
+		})			
 	}, 500);	
 }
 
@@ -49,7 +115,8 @@ function FirstPage () {
 	root.style = ''
 	var element = '<div class="FirstView"><hr/><a href="#RicardoPage">' +
 				  '<div class="word"><span class="Ricardo">Ricardo</span>' +
-				  '</div></a><hr /></div>' 			 
+				  '</div></a><hr /></div>' 	
+				  		 
 	root.innerHTML = element
 	var word = $('.word');
 	word.addEventListener('mouseover', function() {
@@ -65,45 +132,3 @@ function FirstPage () {
 	})
 }
 
-function inserBgm() {
-	// var MusicBox =	'<div style="width: 60px; height: 60px;">'+
-	// 				'<div class="musicPlay" id="MusicCtl">'+
-	// 				'<img src="/image/music.png" width="80%">'+
-	// 				'</div>'+
-	// 				'</div>'
-	var div1 = document.createElement('div'), div2 = document.createElement('div')
-	var img = new Image();
-	img.src = '/image/music.png'
-	img.style.width = '80%'
-	div1.style.width = '60px'
-	div1.style.height = '60px'
-	div2.className = 'musicPlay'
-	div2.id = 'MusicCtl' 
-	div2.appendChild(img)
-	div1.appendChild(div2)
-	root.appendChild(div1)
-	$('#MusicCtl').addEventListener('click', function() {
-	this.className = this.className == 'musicPlay' ? 'musicPause' : 'musicPlay'		
-	if($('#bgm').paused) {
-		$('#bgm').play()
-	} else {
-		$('#bgm').pause()
-	}
-})
-				
-}
-
-function waitWordCss (p, num) {
-	if(num != 0) {
-		setTimeout(function() {
-			num--;
-			p.style.color = (num == 2? 'highlight' : '#7fffd4' )
-			p.innerText = 'Now wait ' + num + ' Second'
-			waitWordCss(p, num)
-		}, 1000)
-	} else {
-		p.style.color = '#dc143c'
-		p.innerText = 'Now Go ahead to see my page'
-	}
-	
-}
